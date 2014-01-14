6b957dfe59ad33ae2874e10b5a9d8a9e828e3219
Chris Mousdale <chris@mousdale.com>
2014-01-14 01:34:26 +0000

    Added in dynamic colouring of button on home page :-)

--- js/cm.js
+++ js/cm.js
@@ -1,10 +1,15 @@
-$('.social-share a').hover (
-  function () {
-    $(".main-footer").css('background-color', $(this).attr("data-color"));
-   },
-  function () {
-    $(".main-footer").css('background-color', '#ddd');
-   }
- );
-
-
+$(document).ready(function() {
+	var opts = {
+		parent: '.btn-c',
+		normalizeTextColor: true
+	};
+	$.adaptiveBackground.run(opts);
+	$('.social-share a').hover (
+	  function () {
+	    $(".main-footer").css('background-color', $(this).attr("data-color"));
+	   },
+	  function () {
+	    $(".main-footer").css('background-color', '#ddd');
+	   }
+	 );
+});
