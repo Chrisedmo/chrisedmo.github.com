307450986eae4c8b5c96bb3b1185f7dbb0a676d6
Chris Mousdale <chris@mousdale.com>
2014-01-21 21:13:04 +0000

    Removed inline JS into the cm.js where possible
    and added in moat comments

--- js/cm.js
+++ js/cm.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+	// Dynamic colours for home page buttons
 	var opts = {
 		parent: '.btn-c',
 		normalizeTextColor: true
 	};
 	$.adaptiveBackground.run(opts);
+	// Dynamic JS Menu
+	$.getJSON('/allPosts.json', function(data)	{
+		var template = '<ul>{{#jsMenu}}<li><a href="{{href}}" title="{{title}}"><h2>{{title}}</h2><p class="date">{{#date}}{{day}} {{month}} {{year}}{{/date}}</p><p>{{short}}</p></a></li>{{/jsMenu}}</ul>';
+		var html = Mustache.to_html(template, data);
+		$('#b-link').click(function(event){
+			event.preventDefault();
+			$('#blogNav').html(html);
+			$('#offNav').addClass('open');
+			$('#b-container').addClass('blur no-scroll');
+		}),
+		$('#offNav').click(function() {
+			$(this).removeClass('open');
+			$('#b-container').removeClass('blur no-scroll');
+		});
+	});
+	// Changing colours of social share background
 	$('.social-share a').hover (
 	  function () {
 	    $(".main-footer").css('background-color', $(this).attr("data-color"));
